8a1d7145fc4d0ff2dbc95051e33f49e12393c54d
Mateus SA <msa.mt314@gmail.com>
2026-04-27 11:59:11 -0400

    refactor: Preenche lógica da Parte 2 e 3

--- main/parte3_analogico_pwm/parte3_analogico_pwm.ino
+++ main/parte3_analogico_pwm/parte3_analogico_pwm.ino
@@ -1,9 +1,26 @@
-void setup() {
-  // put your setup code here, to run once:
+// analogico_pwm.ino
+#define PINO_POT 34
+#define PINO_LED 25
+#define CANAL_PWM 0
+#define FREQ_PWM 1000  // Hz
+#define RESOLUCAO 8    // bits → 0 a 255
 
+void setup() {
+  Serial.begin(115200);
+  ledcSetup(CANAL_PWM, FREQ_PWM, RESOLUCAO);
+  ledcAttachPin(PINO_LED, CANAL_PWM);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  int leitura = analogRead(PINO_POT);       // 0 a 4095 (12 bits)
+  int pwm = map(leitura, 0, 4095, 0, 255);  // remapeia para 8 bits
+  ledcWrite(CANAL_PWM, pwm);
+  float tensao = leitura * (3.3 / 4095.0);
+  Serial.print("ADC: ");
+  Serial.print(leitura);
+  Serial.print(" | V: ");
+  Serial.print(tensao, 2);
+  Serial.print(" | PWM: ");
+  Serial.println(pwm);
+  delay(200);
 }
